11f837994e2608fe8121739e97f93b472cfb5135
KyleFreedomSeeds <35439953+KyleFreedomSeeds@users.noreply.github.com>
2020-09-23 16:51:51 -0700

    Update Reload.java

--- src/main/java/net/dirtcraft/julian/actionbarcoords/Reload.java
+++ src/main/java/net/dirtcraft/julian/actionbarcoords/Reload.java
@@ -1,25 +1,19 @@
 package net.dirtcraft.julian.actionbarcoords;
 
-import com.google.inject.Inject;
+
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.text.serializer.TextSerializers;
-import org.slf4j.Logger;
 
 public class Reload implements CommandExecutor {
 
-    @Inject
-    private Logger logger;
-
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         src.sendMessage(TextSerializers.FORMATTING_CODE.deserialize("&aReloading ActionBar Coords..."));
-	logger.info("Reloading ActionBar Coords...");
         ConfigManager.load();
         src.sendMessage(TextSerializers.FORMATTING_CODE.deserialize("&aActionBar Coords was reloaded successfully!"));
-        logger.info("ActionBar Coords was reloaded successfully!");
         return CommandResult.success();
     }
 
